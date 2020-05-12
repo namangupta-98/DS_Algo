c1d3b3af3e2633a42c960554ba4cf8096aab66ab
namangupta-98 <guptanaman.gupta1@gmail.com>
2020-05-12 09:58:34 +0530

    12/05/20

---
+++ practice_problems/longestSubString.js
@@ -0,0 +1,22 @@
+function findLongestSubstring(str) {
+  let longest = 0;
+  let seen = {};
+  let start = 0;
+ 
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
+    }
+    // index - beginning of substring + 1 (to include current in count)
+    longest = Math.max(longest, i - start + 1);
+    // store the index of the next char so as to not double count
+    seen[char] = i + 1;
+    console.log("i: " + i)
+    console.log("char: " + char)
+    console.log("start: " + start)
+    console.log("longest: " + longest)
+    console.log("seen: " + seen)
+  }
+  return longest;
+}
