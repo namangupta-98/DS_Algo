1a7473397f5bf500447180425fa0af6a7b840652
namangupta-98 <guptanaman.gupta1@gmail.com>
2020-06-11 10:15:34 +0530

    11/06/20

--- Algorithms/Dijkstra.js
+++ Algorithms/Dijkstra.js
@@ -1,24 +1,70 @@
-//Dijkstra'S Algorithm (by Edsger Dijkstra) ~ Shortest Path Algorithm
-//finds shortest path between two vertices on a graph.
-
-//Use:
-//* Maps/Gps
-//* Airline tickets: cheapest route
-//* 
+class Node{
+    constructor(val,priority){
+        this.val = val;
+        this.priority = priority;
+    }
+}
 
 class PriorityQueue{
     constructor(){
         this.values = [];
     }
     enqueue(val,priority){
-        this.values.push({val,priority});
-        this.sort();
+        let newNode = new Node(val,priority);
+        this.values.push(newNode);
+        this.bubbleUp();
+        return this;
+    }
+    bubbleUp(){
+        let idx = this.values.length-1;
+        const element = this.values[idx];
+        while(idx > 0){
+            let parentIdx = Math.floor((idx-1)/2);
+            let parent = this.values[parentIdx];
+
+            if(element.priority >= parent.priority) break;
+
+            this.values[parentIdx] = element;
+            this.values[idx] = parent;
+            idx = parentIdx;
+        }
     }
     dequeue(){
-        return this.values.shift();
+        const min = this.values[0];
+        const end = this.values.pop();
+        if(this.values.length > 0){
+            this.values[0] = end;
+            this.sinkDown();
+        }
+        return min;
     }
-    sort(){
-        this.values.sort((a,b) => a.priority - b.priority);
+    sinkDown(){
+        let idx = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+        while(true){
+            let leftChildIdx = 2 * idx + 1;
+            let rightChildIdx = 2 * idx + 2;
+            let leftChild,rightChild;
+            let swap = null;
+            if(leftChildIdx < length){
+                leftChild = this.values[leftChildIdx];
+                if(leftChild.priority < element.priority){
+                        swap = leftChildIdx;
+                }
+            }
+            if(rightChildIdx < length){
+                rightChild = this.values[rightChildIdx];
+                if((swap===null && rightChild.priority < element.priority)
+                    || (swap!==null && rightChild.priority < leftChild.priority)){
+                        swap = rightChildIdx;
+                }
+            }
+            if(swap===null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+            idx = swap;
+        }
     }
 }
 
@@ -32,6 +78,53 @@ class WeightedGraph{
     addEdge(vertex1,vertex2,weight){
         this.adjacencyList[vertex1].push({node:vertex2,weight});
         this.adjacencyList[vertex2].push({node:vertex1,weight});
+    }
+    dijkstra(start, finish){
+        const nodes = new PriorityQueue();
+        const distances = {};
+        const previous = {};
+        let smallest;
+        let path = [];
+        //initial case
+        for(let vertex in this.adjacencyList){
+            if(vertex === start){
+                distances[vertex] = 0;
+                nodes.enqueue(vertex,0);
+            }else{
+                distances[vertex] = Infinity;
+                nodes.enqueue(vertex,Infinity);
+            }
+            previous[vertex] = null;
+        }
+        while(nodes.values.length){ // until there is some node to visit
+            smallest =  nodes.dequeue().val;
+            if(smallest === finish){
+                // done, build the path and return
+                while(previous[smallest]){
+                    path.push(smallest);
+                    smallest = previous[smallest];
+                }
+                break;
+            }
+            if(smallest || distances[smallest] !== Infinity){
+                for(let neighbor in this.adjacencyList[smallest]){
+                    //find neighboring node
+                    let nextNode = this.adjacencyList[smallest][neighbor];
+                    //calculate distance to the neighboring node
+                    let candidate = distances[smallest] + nextNode.weight;
+                    let nextNeighbor = nextNode.node;
+                    if(candidate<distances[nextNeighbor]){
+                        //new smallest distance
+                        distances[nextNeighbor] = candidate;
+                        //how we got to neighbor
+                        previous[nextNeighbor] = smallest;
+                        // enqueue with new priority
+                        nodes.enqueue(nextNeighbor,candidate);
+                    }
+                }
+            }
+        }
+        return path.concat(smallest).reverse();
     }
 }
 
@@ -51,3 +144,5 @@ graph.addEdge("C","F",5);
 graph.addEdge("D","E",3);
 graph.addEdge("D","F",1);
 graph.addEdge("F","E",1);
+
+graph.dijkstra("A","E");
